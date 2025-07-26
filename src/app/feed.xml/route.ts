f822734d4a7caac1f1b2bb7fed18383feecc59d4
mbablu <mbbiswasbd@yahoo.com>
2025-07-26 09:53:30 +0600

    update

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -3,7 +3,8 @@ import * as cheerio from 'cheerio'
 import { Feed } from 'feed'
 
 export async function GET(req: Request) {
-  let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  // let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
+  let siteUrl = 'www.makhterdesdign.com'
 
   if (!siteUrl) {
     throw Error('Missing NEXT_PUBLIC_SITE_URL environment variable')
